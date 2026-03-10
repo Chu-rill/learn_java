7b192302f3ef2ca44732f1744648cb347dee2d5d
Chu-rill <churchillrimamskep@gmail.com>
2026-03-10 00:18:04 -0400

    learn_java

---
+++ src/test/java/com/learn_java/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_java.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
